cef7a169cc329d56382483f8e4824fe1e4adbad3
Ketut <quentin.abbes@gmail.com>
2020-06-29 17:06:21 +0200

    creating ms-users

--- application/backend/ms-business/ms-users/src/main/java/com/wildadventures/msusers/MsUsersApplication.java
+++ application/backend/ms-business/ms-users/src/main/java/com/wildadventures/msusers/MsUsersApplication.java
@@ -2,8 +2,10 @@ package com.wildadventures.msusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.wildadvenures.msusers")
 public class MsUsersApplication {
 
 	public static void main(String[] args) {
---
+++ application/backend/ms-business/ms-users/src/main/java/com/wildadventures/msusers/model/Address.java
@@ -0,0 +1,5 @@
+package com.wildadventures.msusers.model;
+
+public class Address {
+
+}
