d620f93638d5ebb1c0663ce4bb84033b269fdb78
ibrahima <ibrahimahotia.diallo@gmail.com>
2020-07-08 17:17:25 +0200

    Refactoring

--- application/wild-front/src/app/app.module.ts
+++ application/wild-front/src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { UserService } from "./services/user.service";
+import { OrderService } from "./services/order.service";
+import { AdventureService } from "./services/adventure.service";
+import { CategoryService } from "./services/category.service";
+import { CommentService } from "./services/comment.service";
+import { SessionService } from "./services/session.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    UserService,
+    OrderService,
+    AdventureService,
+    CategoryService,
+    CommentService,
+    SessionService
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
