c94e1efd987b5759080a357dab19fc0847ec58a0
hotiaDiallo <ibrahimahotia.diallo@gmail.com>
2020-07-19 16:16:57 +0200

    Display the number of items in the basket on the header component

--- application/wild-front/src/app/app-routing.module.ts
+++ application/wild-front/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { CategoryComponent } from "./category/category.component";
 import { AdventuresByCategoryComponent } from './adventures-by-category/adventures-by-category.component';
 import { SessionsByAdventureComponent } from './sessions-by-adventure/sessions-by-adventure.component';
 import { HomeComponent } from './home/home.component';
+import { BasketDetailsComponent } from './basket-details/basket-details.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
@@ -14,7 +15,8 @@ const routes: Routes = [
   { path: "adventuresByCategory/:id/adventures/:id/sessions", component: SessionsByAdventureComponent },
   { path : "adventuresByCategory/:id", component : AdventuresByCategoryComponent}, 
   { path: "adventures/:id/sessions", component: SessionsByAdventureComponent }, 
-  { path: "categories", component: CategoryComponent }
+  { path: "categories", component: CategoryComponent },
+  { path: "basket-details", component: BasketDetailsComponent }
 ];
 
 @NgModule({
--- application/wild-front/src/app/header/header.component.ts
+++ application/wild-front/src/app/header/header.component.ts
@@ -6,18 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  public ordersDemand = [];
   isUserLoggedIn : boolean = false;
   
   constructor() { 
     setTimeout(
       () => {
         this.isUserLoggedIn = true;
+        sessionStorage.setItem('userId', JSON.stringify(1))
       }, 10000
     );
   }
 
   ngOnInit() {
+    this.getAllOrderDemandsByUser();
+  }
+
+  getAllOrderDemandsByUser(){
+    this.ordersDemand = JSON.parse(sessionStorage.getItem("orderDemands"));
+    console.log("this.ordersDemand", this.ordersDemand);
   }
 
 }
