fb565048a54b8ed3fb8d3c45c2bf13cd1119ea65
hotiaDiallo <ibrahimahotia.diallo@gmail.com>
2020-08-20 21:52:42 +0200

    Filter to intercept requests and responses

--- application/backend/ms-business/ms-session/src/main/java/com/wildadventures/mssession/proxy/MsAdventureProxy.java
+++ application/backend/ms-business/ms-session/src/main/java/com/wildadventures/mssession/proxy/MsAdventureProxy.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.util.Optional;
 
-@FeignClient(name = "ms-bootstrap.properties", url = "localhost:9001")
+@FeignClient(name = "ms-zuul-server")
 public interface MsAdventureProxy {
 
     @GetMapping(value = "/api/adventures")
